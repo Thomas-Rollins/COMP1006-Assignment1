24101091c7544b9bf0473cd7b886409210229998
Thomas <thomas.rollins@hotmail.ca>
2017-02-01 21:17:34 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en-ca">
+ 	<head>
+ 		<meta charset="utf-8" />
+		<title>COMP1006 Assignment 1</title>
+		<link rel="stylesheet" href="assets/css/styles.css" />
+	</head>
+	<body>
+		<header id="global-nav">
+			<nav id="global">
+				<ul class="nav">
+					<li><a class="active" href="index.php" title="Home Page">Home Page</a></li>
+					<li><a href="todo-list.php" title="Todo List">Todo List</a></li>
+					<li><a href="todo-details.php" title="Todo Details">Todo Details</a></li>
+					<li><a href="login-page.php" title="Login Page">Log In</a></li>
+				</ul>
+			</nav>
+		</header>
+		<main>
+			<section>
+				<article>
+
+				</article>
+			</section>
+		</main>
+	</body>
+</html>
