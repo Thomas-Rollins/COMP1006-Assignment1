e6ea11f506434c9553674cfb695f445544678d9a
Thomas-Rollins <thomas.rollins@hotmail.ca>
2017-02-21 18:31:09 -0500

    connection string test

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
 include_once('http://comp1006-assignment1.azurewebsites.net/assets/php/session-info.php');
+// test
+$dsn = 'mysql:us-cdbr-azure-northcentral-b.cloudapp.net;dbname=comp1006_assignment1';
+$userName = 'b9e8f2c430a4ad';
+$password = 'b54eca3a';
+
+
+try {
+    // instantiates a new pdo - an db object
+    $db = new PDO($dsn, $userName, $password);
+}
+catch(PDOException $e) {
+    $message = $e->getMessage();
+    echo "An error occurred: " . $message;
+}
+
+$query = "SELECT * FROM todo_list WHERE userID =:userid ORDER BY id ASC"; // SQL statement
+$statement = $db->prepare($query); // encapsulate the sql statement
+$statement->bindValue(':userid', $login_session);
+$statement->execute(); // run on the db server
+$todo = $statement->fetchAll(); // returns an array
+$statement->closeCursor(); // close the connection
 ?>
 <!DOCTYPE html>
  <html lang="en-ca">
@@ -36,6 +57,56 @@ include_once('http://comp1006-assignment1.azurewebsites.net/assets/php/session-i
           }
             ?>
 				</article>
+
+
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+              <table class="table table-inverse">
+                <thread>
+                  <tr>
+                    <th>Name</th>
+                    <th>Notes</th>
+                    <th>Completed</th>
+                    <th> </th>
+                    <th> </th>
+            </tr>
+          </thread>
+          <tbody>
+            <?php foreach($todo as $todo) : ?>
+                <tr>
+                  <?php if($todo['completed'] == 1){
+                    echo('</del><td><del>'); // strikes through text if listed as completed
+                  } else {
+                    echo('<td>');
+                  }?><?php echo $todo['name'] ?></td>
+                  <?php if($todo['completed'] == 1){
+                    echo('</del><td><del>');
+                  } else {
+                    echo('<td class=\'notes\'>');
+                  }?><?php echo $todo['notes'] ?></td>
+                  <?php if($todo['completed'] == 1){
+                    echo('</del><td class=\'complete\'>');
+                  } else {
+                    echo('<td>');
+                  }?><?php if($todo['completed'] == 1){
+                    echo('YES');
+                  } else {
+                    echo('NO');
+                  }?></td>
+                    <td class="edit"><a class="btn btn-warning" href="http://comp1006-assignment1.azurewebsites.net/todo-details.php?todo_id=<?php echo $todo['id'] ?>"><i class=""></i> Edit</a></td>
+                    <td class="delete"><a class="btn btn-danger" href="http://comp1006-assignment1.azurewebsites.net/assets/php/delete-db-entry.php?todo_id=<?php echo $todo['id'] ?>"><i class=""></i> Delete</a></td>
+                </tr>
+            <?php endforeach; ?>
+            <tr>
+              <td colspan="5"><a class="btn btn-primary" id="newEntry" href="http://comp1006-assignment1.azurewebsites.net/todo-details.php?todo_id=0">Add new Entry</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 			</section>
 		</main>
 	</body>
