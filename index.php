7db324d42631155c650d0d3210e7a8cc94470f7d
Thomas-Rollins <thomas.rollins@hotmail.ca>
2017-02-21 19:19:32 -0500

    test

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include_once('http://comp1006-assignment1.azurewebsites.net/session-info.php');
+include_once('database.php');
 ?>
 <!DOCTYPE html>
  <html lang="en-ca">
@@ -37,6 +38,64 @@ include_once('http://comp1006-assignment1.azurewebsites.net/session-info.php');
           }
             ?>
 				</article>
+
+        <?php
+        $query = "SELECT * FROM todo_list WHERE userID =:userid ORDER BY id ASC"; // SQL statement
+        $statement = $db->prepare($query); // encapsulate the sql statement
+        $statement->bindValue(':userid', $login_session);
+        $statement->execute(); // run on the db server
+        $todo = $statement->fetchAll(); // returns an array
+        $statement->closeCursor(); // close the connection
+         ?>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+              <table class="table table-inverse">
+                <thread>
+                  <tr>
+                    <th>Name</th>
+                    <th>Notes</th>
+                    <th>Completed</th>
+                    <th> </th>
+                    <th> </th>
+            </tr>
+          </thread>
+          <tbody>
+            <?php foreach($todo as $todo) : ?>
+                <tr>
+                  <?php if($todo['completed'] == 1){
+                    echo('</del><td><del>'); // strikes through text if listed as completed
+                  } else {
+                    echo('<td>');
+                  }?><?php echo $todo['name'] ?></td>
+                  <?php if($todo['completed'] == 1){
+                    echo('</del><td><del>');
+                  } else {
+                    echo('<td class=\'notes\'>');
+                  }?><?php echo $todo['notes'] ?></td>
+                  <?php if($todo['completed'] == 1){
+                    echo('</del><td class=\'complete\'>');
+                  } else {
+                    echo('<td>');
+                  }?><?php if($todo['completed'] == 1){
+                    echo('YES');
+                  } else {
+                    echo('NO');
+                  }?></td>
+                    <td class="edit"><a class="btn btn-warning" href="http://comp1006-assignment1.azurewebsites.net/todo-details.php?todo_id=<?php echo $todo['id'] ?>"><i class=""></i> Edit</a></td>
+                    <td class="delete"><a class="btn btn-danger" href="http://comp1006-assignment1.azurewebsites.net/delete-db-entry.php?todo_id=<?php echo $todo['id'] ?>"><i class=""></i> Delete</a></td>
+                </tr>
+            <?php endforeach; ?>
+            <tr>
+              <td colspan="5"><a class="btn btn-primary" id="newEntry" href="http://comp1006-assignment1.azurewebsites.net/todo-details.php?todo_id=0">Add new Entry</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      </div>
+      </div>
+
 			</section>
 		</main>
 	</body>
